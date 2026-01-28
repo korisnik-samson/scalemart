81396f0b0f556127187027142d3ea4203f3e3c32
Samson <126519078+korisnik-samson@users.noreply.github.com>
2026-01-28 22:00:53 +0100

    Initial Commit

---
+++ src/main/java/com/samson/scalemart/ScalemartApplication.java
@@ -0,0 +1,13 @@
+package com.samson.scalemart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScalemartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScalemartApplication.class, args);
+    }
+
+}
